1fd1119965d7c6e554f7e0a57d629f86195dff0b
Smilez/Get-Exploits <smilezrealscripts@gmail.com>
2025-03-12 14:41:14 -0400

    Update Blacklisted.lua

--- Blacklisted.lua
+++ Blacklisted.lua
@@ -7,7 +7,6 @@ game.Players.PlayerAdded:Connect(function()
 	for _, v in pairs(game.Players:GetChildren()) do
 		for i = 1, #banned do
 			if v.UserId == banned[i] then
-				warn("Oof, Your Blacklisted.")
 				v:Kick("You Have Been Blacklisted.") -- You may set a custom message within the quotations
 			end
 		end
